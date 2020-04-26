274785e22a9ca9f7a45999ab8c92a791c99c3867
Hardeep Singh <54856029+HardeepSiyan@users.noreply.github.com>
2020-04-26 16:36:46 -0400

    Setup Routes fo Settings and About closes #28

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from "react-dom";
 
 /* Page Imports */
 import Dashboard from 'Pages/Dashboard/Dashboard';
+import Settings from 'Pages/Settings/Settings';
+import About from './js/Pages/About/About';
+
+// Router Imports
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 import {Store} from "./js/Store";
 
@@ -12,7 +17,12 @@ import 'css/index.css';
 const App = () => {
 	return (
         <Store>
-            <Dashboard />
+            <Router>
+                <Route exact path="/" component={Dashboard} />
+                <Route path="/settings" component={Settings} />
+                <Route path="/about" component={About} />
+            </Router>
+            
         </Store>
     )
 }
