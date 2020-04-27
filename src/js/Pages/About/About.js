c791c4b9f17cd34824a9185c1af3195e04cdae02
Hardeep Singh <54856029+HardeepSiyan@users.noreply.github.com>
2020-04-26 20:45:34 -0400

    Developed About Page

--- src/js/Pages/About/About.js
+++ src/js/Pages/About/About.js
@@ -3,7 +3,23 @@ import React from "react";
 const About = () => {
     return (
         <>
-           <h1>About</h1>
+        <header class="settingsHeader">
+            <span class="material-icons homeIconHeader">home</span>
+        </header>
+        <main>
+            <section class="about">
+            <div id="AboutTab">
+                <div class="aboutTile">
+                    {/* <img src="/svg/Logo.svg" alt="Logo Outer Circle" class="aboutImg"> */}
+                    <h3>OuterCircle</h3>
+                </div>
+                <h4>A Chat Website.</h4>
+                <h4>Created By: - </h4>
+                <a href="https://kunaldhawan93.com/" target="_blank"><h4>Kunal Dhawan</h4></a>
+                <a href="https://hardeepsiyan.com/" target="_blank"><h4>Hardeep Singh</h4></a>
+            </div>
+            </section>
+        </main>
         </>
     )
 }
